1d49c2f20e78a34e03255337d1db965a65a56411
Đỗ Hỷ Tài <taido.88241020248@st.ueh.edu.vn>
2025-03-25 15:45:24 +0700

    Add files via upload

---
+++ lib/flutter_platform_widgets.dart
@@ -0,0 +1,53 @@
+/*
+ * flutter_platform_widgets
+ * Copyright (c) 2018 Lance Johnstone. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+library flutter_platform_widgets;
+
+export 'src/platform.dart'
+    show
+        isCupertino,
+        isMaterial,
+        platform,
+        platformThemeData,
+        showPlatformDialog,
+        showPlatformModalSheet,
+        MaterialModalSheetData,
+        CupertinoModalSheetData,
+        MaterialDialogData,
+        CupertinoDialogData,
+        PlatformTarget;
+export 'src/platform_alert_dialog.dart';
+export 'src/platform_app.dart';
+export 'src/platform_app_bar.dart';
+export 'src/platform_checkbox.dart';
+export 'src/platform_circular_progress_indicator.dart';
+export 'src/platform_date_picker.dart';
+export 'src/platform_dialog_action.dart';
+export 'src/platform_elevated_button.dart';
+export 'src/platform_icon_button.dart';
+export 'src/platform_icons.dart';
+export 'src/platform_list_tile.dart';
+export 'src/platform_nav_bar.dart';
+export 'src/platform_page.dart';
+export 'src/platform_page_route.dart';
+export 'src/platform_popup_menu.dart';
+export 'src/platform_provider.dart';
+export 'src/platform_radio.dart';
+export 'src/platform_scaffold.dart';
+export 'src/platform_scrollbar.dart';
+export 'src/platform_search_bar.dart';
+export 'src/platform_slider.dart';
+export 'src/platform_sliver_app_bar.dart';
+export 'src/platform_switch.dart';
+export 'src/platform_tab_scaffold.dart';
+export 'src/platform_text.dart';
+export 'src/platform_text_button.dart';
+export 'src/platform_text_field.dart';
+export 'src/platform_text_form_field.dart';
+export 'src/platform_theme.dart';
+export 'src/platform_widget.dart';
+export 'src/platform_widget_builder.dart';
+export 'src/widget_base.dart';
